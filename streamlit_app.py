ca8bc08e2c39a55fc6613f3849e6fe239c1d7592
Manoj Darshan R <73886885+ManojDarshanR@users.noreply.github.com>
2024-02-12 11:11:29 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,7 +44,7 @@ streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
 # allow the end user to add a new fruit to the table 
-add_my_fruit = streamlit.text\_input("Enter a new fruit:")
+add_my_fruit = streamlit.text_input("Enter a new fruit:")
 if new\_fruit:
     my_data_rows = my_data_rows.append({'fruit': new\_fruit}, ignore\_index=True)
 streamlit.dataframe(my_data_rows)
