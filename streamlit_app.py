17b8f8c0b80df76975ae564d7b07e2c434ffe228
Manoj Darshan R <73886885+ManojDarshanR@users.noreply.github.com>
2024-02-12 11:56:56 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,9 @@
 import streamlit
+import pandas
+import requests
+import snowflake.connector
+from urllib.error import URLError
+
 streamlit.title("My Parents New Healthy Diner")
 
 streamlit.header('Breakfast Menu')
@@ -9,7 +14,6 @@ streamlit.text('🥑🍞Avacado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
@@ -24,7 +28,6 @@ streamlit.dataframe(fruits_to_show)
 # New Section to display fruitvice api response
 streamlit.header("Fruityvice Fruit Advice!")
 
-import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
@@ -34,7 +37,7 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # output it the screen as a table
 streamlit.dataframe(fruityvice_normalized)
 
-import snowflake.connector
+streamlit.stop()
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
@@ -46,7 +49,7 @@ streamlit.dataframe(my_data_rows)
 # allow the end user to add a new fruit to the table 
 add_my_fruit = streamlit.text_input("Enter a new fruit:")
 if add_my_fruit:
-    my_data_rows = my_data_rows.append({'fruit': add_my_fruit}, ignore_index=True)
+    my_data_rows = my_data_rows.append({'Fruit': add_my_fruit}, ignore_index=True)
 streamlit.dataframe(my_data_rows)
 
 streamlit.write('Thanks for adding ', add_my_fruit)
